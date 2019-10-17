3baad044e4550e2af52ff5962410ab3388c8e3b7
daveeclark <dclark2016@hotmail.com>
2019-10-17 10:53:04 -0400

    this shit ass

--- src/main/java/klondikeGame/Klondike.java
+++ src/main/java/klondikeGame/Klondike.java
@@ -8,7 +8,7 @@ import java.util.*;
 
 public class Klondike extends DiceGame implements Igamblinggame, GamblingPlayer {
 
-  Klondike() {
+  public Klondike() {
     this.setNumOfDice(5);
   }
 
@@ -35,41 +35,52 @@ public class Klondike extends DiceGame implements Igamblinggame, GamblingPlayer 
 
   public String play() {
     Scanner scanner = new Scanner(System.in);
+
     System.out.println("Place bet on Win or Lose");
     String userMarkedBet = scanner.nextLine();
     System.out.println("You placed a bet on " + userMarkedBet);
     System.out.println("Place your bet amount");
     int userBetAmount = scanner.nextInt();
     System.out.println("You bet " + userBetAmount);
+    String continuePlaying = scanner.nextLine();
 
-
-    while (totalAmount > placeBet){
-      if (totalAmount > userBetAmount){
-        return "Let's begin";
-      } else {
-        return "You don't have enough money cuz";
+    while (totalAmount > 0) {
+      System.out.println("You have " + GamblingPlayer.totalAmount + ". Let's begin.");
+      if (playerBet > GamblingPlayer.totalAmount) {
+        return "Get tf outta here broke azz";
+        break;
       }
+
+      String playerWins = "Congrats you won " + userBetAmount + "and have a total bank of " + (userBetAmount + GamblingPlayer.totalAmount)
+        + ". /n" + "Continue playing? Type 1 for yes, or 2 for no";
+
+      String playerLoses = "Nope, try again, you lost " + userBetAmount + "and have " +
+        (GamblingPlayer.totalAmount - userBetAmount) + " left.";
+
+      while (true){
+        if (continuePlaying.contains("2")){
+
+      } else if (userMarkedBet.contains("Win") && playerRoll() > bankerRoll()) {
+        return playerWins + continuePlaying;
+      } else if (userMarkedBet.contains("Win") && playerRoll() < bankerRoll()) {
+        return playerLoses + continuePlaying;
+      } else if (userMarkedBet.contains("Lose") && playerRoll() < bankerRoll()) {
+        return playerWins + continuePlaying;
+      } else if (userMarkedBet.contains("Lose") && playerRoll() > bankerRoll()) {
+        return playerLoses + continuePlaying;
+      } else if (playerRoll() == bankerRoll()) {
+        return playerLoses + continuePlaying;
+      } else if (continuePlaying.contains("2")) {
+        return "Thanks for playing! Please check out our other games around the casino and enjoy our speciality juices!";
+      } else {
+          return "You don't have enough money cuz";
+        }
+      }
+    return "Thank you come again!";
     }
 
-    String bankerWins = "Nope, try again, you lost " + userBetAmount + "and have " + totalAmount + " left.";
-    String playerWins = "Congrats you won "  + (userBetAmount + totalAmount);
 
 
-    if (userMarkedBet.contains("Win") && playerRoll() > bankerRoll()) {
-      return playerWins;
-    } else if(userMarkedBet.contains("Win") && playerRoll() < bankerRoll()){
-      return bankerWins;
-    } else if (userMarkedBet.contains("Lose") && playerRoll() < bankerRoll()) {
-      return playerWins;
-    } else if (userMarkedBet.contains("Lose") && playerRoll() > bankerRoll()){
-      return bankerWins;
-    } else if (playerRoll() == bankerRoll() ) {
-      return bankerWins;
-    } else {
-      return "Thanks for playing";
-    }
-
-  }
 
   public void placeBet(int betAmount) {
 
@@ -78,4 +89,6 @@ public class Klondike extends DiceGame implements Igamblinggame, GamblingPlayer 
   public void nextTurn() {
 
   }
+
+
 }
--- src/test/java/io/zipcoder/casino/CasinoTest.java
+++ src/test/java/io/zipcoder/casino/CasinoTest.java
@@ -1,5 +1,12 @@
 package io.zipcoder.casino;
 
 
+import klondikeGame.Klondike;
+
 public class CasinoTest {
+
+  public static void main(String[] args) {
+    Klondike klondike = new Klondike();
+    klondike.play();
+  }
 }
